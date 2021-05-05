6e2465d113a19668de3354fb784a2e7df8d05223
hieutrung25 <hieunt25051999@gmail.com>
2021-05-05 23:11:22 +0700

    java

---
+++ DemoJava/src/Next.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Next {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = 10;
+        System.out.println("x:" +x);
+        String s = sc.next();
+        System.out.println("s: "+ "abc" +s);
+//        String d = sc.nextLine();
+//        System.out.println("d:" + d);
+    }
+}
